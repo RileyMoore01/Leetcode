d0dea09b69969b41ec451cb6ca408df695ae733c
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2021-11-02 11:13:05 -0500

    Create RotateImage.java

---
+++ Array/RotateImage.java
@@ -0,0 +1,27 @@
+/*
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int[][] result = new int[matrix.length][matrix[0].length];
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                result[j][matrix.length - i - 1] = matrix[i][j];
+            }    
+        }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = result[i][j];
+            }
+        }
+    }
+}
