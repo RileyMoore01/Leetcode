cd8380f77376adcd2781bc148110de35b29f8c45
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2021-12-12 22:17:05 -0600

    Update StringCompression.java

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -16,3 +16,21 @@ String compressedBad(String str) {
     }
     return compressedString.length() < str.length() ? compressedString : str;
 }
+
+//------------------------------------------------------------------------------------
+//Same approach using StringBuilder
+String compressedBad(String str) {
+    StringBuilder compressed = new StringBuilder();
+    int countConsecutive = 0;
+    for(int i = 0; i < str.length(); i++) {
+        countConsecutive++;
+
+        //if the next character is different than current character
+        if(i + 1 >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+            compressed.append(str.charAt(i));
+            compressed.append(countConsecutive);
+            countConsecutive = 0;
+        }
+    }
+    return compressed.length() < str.length() ? compressed.toString() : str;
+}
