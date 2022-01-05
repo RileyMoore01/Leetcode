57e7d1a033967eabb98c91de2f959a3e39d940a7
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-01-05 11:49:32 -0600

    Update RemoveDups.java

--- LinkedLists/RemoveDups.java
+++ LinkedLists/RemoveDups.java
@@ -20,3 +20,22 @@ void deleteDups(LinkedListNode n) {
         n = n.next;
     }
 }
+
+//-----------------------------------------------------------------
+
+//Without the temporary buffer
+
+    void deleteDulps(LinkedListNode n) {
+        LinkedListNode current = head;
+        while(current != null) {
+            LinkedListNode runner = current;
+            while(runner.next != null) {
+                if(runner.next.data == current.data) {
+                    runner.next = runner.next.next;
+                } else {
+                    runner = runner.next;
+                }
+            }
+            current = current.next;
+        }
+    }
