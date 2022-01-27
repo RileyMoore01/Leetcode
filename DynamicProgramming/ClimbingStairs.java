7c7b1d92ffc4af0a4367b54e8f3cf14afb480bfc
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-01-27 16:01:36 -0600

    Update ClimbingStairs.java

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -1,18 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n >= 0 && n <=3) return n;
+        if(n < 3) return n;   
         
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,0);
-        for(int i =0;i<n;i++ ){
-            if (i-1 > -1){
-                dp[i] += 1+dp[i-1];
+        int[] arr = new int[n+1];   //This will be the returned result array
+        Arrays.fill(arr, 0);        //Temporarily fill dp will 0's
+        for(int i = 0; i < n; i++) {
+            if(i-1 > -1){
+                arr[i] += 1 + arr[i-1]; //Add another step course if past step exisit for one step progression
+            } else if (i-2 > -1) {
+                arr[i] += 1 + arr[i-2]; //Add another step course if the second previous step exisit for two step progression
             }
-            if (i-2 > -1){
-               dp[i] += 1+ dp[i-2]; 
-            }
-            
         }
-        return dp[n-2]+2;
+        return arr[n-2]+2;
     }
 }
