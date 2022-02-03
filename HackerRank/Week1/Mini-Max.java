d74f12bf3fa7baf23fcbbb0f8497a845dfa85da6
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-03 10:30:10 -0600

    Create Mini-Max.java

---
+++ HackerRank/Week1/Mini-Max.java
@@ -0,0 +1,72 @@
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+        long max = 0, min = 0;
+        Collections.sort(arr);
+        for(int i = 1; i < arr.size(); i++) {
+            max += arr.get(i);
+        }
+        for(int j = 0; j < arr.size() - 1; j++) {
+            min += arr.get(j);
+        }
+        
+        System.out.println(min + " " + max);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
