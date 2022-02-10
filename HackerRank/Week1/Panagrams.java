8b588ad793cefc0131e49fede0be9473878fec0e
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-10 17:31:15 -0600

    Create Panagrams.java

---
+++ HackerRank/Week1/Panagrams.java
@@ -0,0 +1,61 @@
+/*
+A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+Example
+The string contains all letters in the English alphabet, so return pangram.
+
+Function Description
+
+Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+pangrams has the following parameter(s):
+
+string s: a string to test
+Returns
+string: either pangram or not pangram
+Input Format
+A single line with string .
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+    // Write your code here
+        return s.toLowerCase().replace(" ", "").chars().distinct().count() == 26 ? "pangram" : "not pangram";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
