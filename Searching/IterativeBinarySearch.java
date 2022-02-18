009d4522d974ec180ea8b05bca30a471e95c6b71
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-17 19:15:52 -0600

    Update and rename IterativeBinarySearch to IterativeBinarySearch.java

--- Searching/IterativeBinarySearch.java
+++ Searching/IterativeBinarySearch.java
@@ -39,3 +39,22 @@ class BinarySearch
      }
   }
 }
+
+
+----------------------------------------------------------------------------
+//Short Soultion
+
+int start = 0, pivot;
+        int end = nums.length - 1;
+        while (start <= end) {
+            pivot = start + (end - start) / 2;
+            if(nums[pivot] == target)
+                return pivot;
+            if(target < nums[pivot])
+                end = pivot - 1;
+            else 
+                start = pivot + 1;
+        }
+        return -1;
+   }
+}
