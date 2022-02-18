8e3ed6239784e84f606bf4e94a1de6336aca685c
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-18 16:07:11 -0600

    Create SearchInsertPosition.java

---
+++ Array/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++)
+        // If K is found
+            if (nums[i] == target)
+                return i; 
+        // If current array element exceeds K
+            else if (nums[i] > target)
+                return i;
+ 
+        // If all elements are smaller than K
+        return nums.length;
+    }
+}
