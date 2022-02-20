d66afe40c4aa4de6888e3c6194118bc26a58e5bd
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-19 23:53:58 -0600

    Create StackBasics.java

---
+++ Stack/StackBasics.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+import java.util.Iterator;
+
+public class Stack {
+    public static void main(String[] args) {
+        //Creating an instance of Stack class
+        Stack<Integer> stk = new Stack<>();
+
+        //Checking if stack is empty
+        boolean result = stk.empty();
+        System.out.println("Is the stack empty? " + result);
+
+        //Pushing elements
+        stk.push(78);  
+        stk.push(113);  
+        stk.push(90);  
+        stk.push(120);  
+
+        //prints elements of the stack  
+        System.out.println("Elements in Stack: " + stk);  
+        result = stk.empty();  
+        System.out.println("Is the stack empty? " + result);  
+
+        //Iterate & Print stack
+        Iterator iterator = stk.iterator();
+        while(iterator.hasNext()) {
+            Object values = iterator.next();
+            System.out.println(values);
+        }
+
+        //Popping all elements
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        stk.pop();
+
+        //Ensuring stack is empty
+        System.out.println("Elements in Stack: " + stk);  
+        result = stk.empty();  
+        System.out.println("Is the stack empty? " + result);  
+    } 
+}
