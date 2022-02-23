53a61202ad3584dd6a354dc8aac4ea45a60ade57
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-23 10:33:13 -0600

    Update RemoveNthNode.java

--- LinkedLists/RemoveNthNode.java
+++ LinkedLists/RemoveNthNode.java
@@ -1,6 +1,7 @@
 /*
-
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
 */
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
