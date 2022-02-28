005b53b02305ea3f5ff030155e292c13d53057c2
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-02-28 13:50:28 -0600

    Update checkPermutation.java

--- Strings/checkPermutation.java
+++ Strings/checkPermutation.java
@@ -1,4 +1,33 @@
 public class leet {
+    //If s1 is in s2 of any form return true, otherwise false
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length())
+            return false;
+        HashMap<Character, Integer> s1map = new HashMap<>();
+        
+        for(int i = 0; i < s1.length(); i++)
+            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i),0) +1);
+        
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            HashMap <Character, Integer> s2map = new HashMap<> ();
+            for (int j = 0; j < s1.length(); j++) {
+                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
+            }
+            if (matches(s1map, s2map))
+                return true;
+        }
+        return false;
+    }
+    
+    public boolean matches(HashMap <Character, Integer> s1map, HashMap <Character, Integer> s2map) {
+        for (char key: s1map.keySet()) {
+            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0)
+                return false;
+        }
+        return true;
+    }
+        
+    //-------------------------------------------------------------------------------------------
 
     //Questions to ask interviewer:
     //  -If the problem is case sensitive. Is God a permutation of dog?
@@ -19,7 +48,7 @@ public class leet {
         return sort(s).equals(sort(t));
     }
 
-    //-------------------------------------
+    //----------------------------------------------------------------------------------------
     //Solution #2: Check if 2 strings have same character counts
     boolean permutation(String s, String t) {
         if(s.length() != t.length()) 
