d0b61831256b52abd54a2f4edc1d36ce6cc4b021
zebu-lord <50551379+zebu-lord@users.noreply.github.com>
2022-03-16 15:57:07 -0500

    Update AdventureTime.cs

--- C#/AdventureTime.cs
+++ C#/AdventureTime.cs
@@ -20,27 +20,36 @@ namespace ChooseYourOwnAdventure
       Console.WriteLine("Type: YES or NO?");
       string noiseChoice = Console.ReadLine();
       noiseChoice = noiseChoice.ToUpper();
-      if(noiseChoice == "NO") {
+      if(noiseChoice == "NO") 
+      {
         Console.WriteLine("Not much of an adventure if we don't leave our room!");
         Console.WriteLine("THE END.");
-      } else if(noiseChoice == "YES") {
+      } 
+      else if(noiseChoice == "YES") 
+      {
         Console.WriteLine("You walk into the hallway and see a light coming from under a door down the hall.");
         Console.WriteLine("You walk towards it. Do you open it or knock?");
       }
       Console.WriteLine("Type: OPEN or KNOCK");
       string doorChoice = Console.ReadLine();
       doorChoice = doorChoice.ToUpper();
-      if(doorChoice == "KNOCK") {
+      if(doorChoice == "KNOCK") 
+      {
         Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle: \" \"Poor people have it. Rich people need it. If you eat it you die. What is it?\"");
         Console.Write("Type your answer: ");
         string riddleAnswer = Console.ReadLine();
         riddleAnswer = riddleAnswer.ToUpper();
-        if(riddleAnswer == "NOTHING") {
+        if(riddleAnswer == "NOTHING") 
+        {
           Console.WriteLine("The door opens and NOTHING is there. You turn off the light and run back to your room and lock the door.THE END.");
-        } else {
+        } 
+        else 
+        {
           Console.WriteLine("You answered incorrectly. The door doesn't open.THE END.");
         }
-      } else if(doorChoice == "OPEN") {
+      } 
+      else if(doorChoice == "OPEN") 
+      {
           Console.WriteLine("The door is locked! See if one of your three keys will open it.");
           Console.Write("Enter a number (1-3):");
           string keyChoice = Console.ReadLine();
