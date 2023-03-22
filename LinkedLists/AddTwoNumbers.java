1529b9735427f0d19dddc393a04775c94ea59aba
Riley Moore <50551379+RileyMoore01@users.noreply.github.com>
2023-03-22 16:23:26 -0500

    Update AddTwoNumbers.java

--- LinkedLists/AddTwoNumbers.java
+++ LinkedLists/AddTwoNumbers.java
@@ -40,22 +40,26 @@ class Solution {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carryOver = 0;
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-
-        while (l1 != null || l2 != null || carryOver != 0) {
-            int value1 = (l1 != null) ? l1.val : 0;
-            int value2 = (l2 != null) ? l2.val : 0;
-            int sum = carryOver + value1 + value2;
-            carryOver = sum / 10;
-            current.next = new ListNode(sum % 10);
-            current = current.next;
+        int carry = 0;
+        ListNode tempNode = new ListNode(0);
+        ListNode currentNode = tempNode;
+        
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int holder1 = (l1 != null) ? l1.val : 0;
+            int holder2 = (l2 != null) ? l2.val : 0;
+            int sum = holder1 + holder2 + carry;
+            
+            carry = sum / 10;
+            currentNode.next = new ListNode(sum % 10);
+            currentNode = currentNode.next;
+            
             if (l1 != null)
                 l1 = l1.next;
             if (l2 != null)
                 l2 = l2.next;
         }
-        return dummy.next;
+        
+        return tempNode.next;
     }
 }
