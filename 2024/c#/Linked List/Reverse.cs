bc04d47568b0f4887251694d1b33945d3dc0ce94
Riley Moore <50551379+RileyMoore01@users.noreply.github.com>
2024-09-09 06:01:29 -0500

    Update Reverse.cs

--- 2024/c#/Linked List/Reverse.cs
+++ 2024/c#/Linked List/Reverse.cs
@@ -1,1 +1,12 @@
 /* Here we will take a singly, doubly, and circular linked list and reverse it */
+using system;
+
+public class Node () {
+  public int data;
+  public Node next;
+
+  public Node(int data) {
+    this.data = data;
+    this.next = null;
+  }
+}
