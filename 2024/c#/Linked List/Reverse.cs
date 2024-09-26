a8b5397a605002d7eac70a3b834b9d290a7ad9de
Riley Moore <50551379+RileyMoore01@users.noreply.github.com>
2024-09-26 16:21:42 -0500

    Update Reverse.cs

--- 2024/c#/Linked List/Reverse.cs
+++ 2024/c#/Linked List/Reverse.cs
@@ -1,18 +1,43 @@
-/* Here we will take a singly, doubly, and circular linked list and reverse it */
-using system;
-
-public class Node () {
-  public int data;
-  public Node next;
-
-  public Node(int data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-public class LinkedList () {
-  public Node head;
-
-  // Function to add a node at the end of a linked list
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        // edge case
+        if (head == null)
+            return null;
+        
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode current = head;
+        
+        // push all nodes onto the stack
+        while (current != null)
+        {
+            stack.Push(current);
+            current = current.next;
+        }
+        
+        // Pop ndoes from teh stack to create the reversed list
+        ListNode newHead = stack.Pop();
+        ListNode currentNew = newHead;
+        
+        while(stack.Count > 0) 
+        {
+            currentNew.next = stack.Pop();
+            currentNew = currentNew.next;
+        }
+        
+         // set the next of the last node to null to terminate the list
+         currentNew.next = null;
+        
+        return newHead;
+    }
 }
